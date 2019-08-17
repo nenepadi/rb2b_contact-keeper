e0ec3049a845f15cddd294a723d0c7d75ce5a098
Nene Padi Ademang <obed.ademang@gmail.com>
2019-08-17 04:58:44 +0000

    User creation and token generation

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const connect = require('./config/db');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 connect();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Contact Keeper API!' });
