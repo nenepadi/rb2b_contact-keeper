e5b48206fe27ed69af14511e4214fc9cf13d72a4
Nene Padi Ademang <obed.ademang@gmail.com>
2019-08-20 06:32:16 +0000

    Frontend started

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = (req, res, next) => {
+module.exports = function(req, res, next) {
     const token = req.header('x-auth-token');
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied!' });
@@ -12,6 +12,6 @@ module.exports = (req, res, next) => {
         req.user = decoded.user;
         next();
     } catch (err) {
-        resizeTo.status(401).json({ msg: 'Token is not valid!' });
+        res.status(401).json({ msg: 'Token is not valid!' });
     }
 };
