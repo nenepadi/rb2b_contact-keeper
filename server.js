9f131ca939fc75ef1752358f69b50a8e1bda5a0e
Nene Padi Ademang <obed.ademang@gmail.com>
2019-09-21 19:01:14 +0000

    Deployment ready

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connect = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -14,5 +15,12 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
